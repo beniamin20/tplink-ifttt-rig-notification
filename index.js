20bf2ae481caf4213576e43584e55a0585b60d68
Beni <beni@wearewip.com>
2018-05-11 14:36:43 +0300

    updated js file + npm files

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 // npm install iftttmaker               - https://github.com/kolarcz/node-iftttmaker
 // npm install --save tplink-cloud-api  - https://github.com/adumont/tplink-cloud-api
 
+// IFTTT Data
 const apiKey = '<IFTTTMaker apiKey>';
-const event = '<IFTTT event name>';
-const timeInterval = 30000; // time interval for power usage check
-const powerValue = 600; // minimum power consumption
+const event  =  '<IFTTTMaker event>';
+
+// TPLink Data
+const tpLinkAccount = '<TPLink account>';                    // Kasa account
+const tpLinkAccountPassword = '<TPLink password>';           // Kasa password
+const tpLinkDeviceId = '<UUID4 deviceId>';                   // A generated UUID4
+const tpLinkPlugDeviceName = '<TPLink Device Name>';         // Smart plug name
+
+// Miner Data
+const timeInterval = 30000;     // time interval for power usage check in ms 
+const powerValue = 600;         // minimum power consumption in W
+
+// =====================================================================================
 
 var TPLink = require('tplink-cloud-api')
 var IFTTTMaker = require('iftttmaker')(apiKey);
@@ -14,14 +25,14 @@ var myTPLink;
 async function start() {
   let response
 
-  myTPLink = await TPLink.login('<IFTTTMaker account>', '<IFTTTMaker password>', '<UUID4 deviceId>');
+  myTPLink = await TPLink.login(tpLinkAccount, tpLinkAccountPassword, tpLinkDeviceId);
 
   console.log( myTPLink.getToken() )
   var dl = await myTPLink.getDeviceList();
 console.log('Device List ==================')
   console.log( dl );
 
-  var myPlug = myTPLink.getHS100("<TPLink Device Name>");
+  var myPlug = myTPLink.getHS100(tpLinkPlugDeviceName);
 
   console.log("Rig Plug deviceId = " + myPlug.getDeviceId())
 
@@ -41,7 +52,7 @@ async function poolData() {
 }
 
 async function checkConsumption() {
-    var energyData = await myTPLink.getHS110("<TPLink Device Name>").getPowerUsage();
+    var energyData = await myTPLink.getHS110(tpLinkPlugDeviceName).getPowerUsage();
     var consumption = energyData.power_mw / 1000
     console.log(consumption);
 
@@ -51,7 +62,7 @@ async function checkConsumption() {
 }
 
 async function sendNotification() {
-  IFTTTMaker.send('rig_down').then(function () {
+  IFTTTMaker.send(event).then(function () {
   console.log('> Request was sent');
 
 }).catch(function (error) {
